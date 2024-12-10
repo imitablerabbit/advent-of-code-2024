b5d8cfd1c1a9fcc121cd82a4ad9760f4739b0933
Mark Hillman <mark@markhillman.info>
2024-12-10 19:05:36 +0000

    Updated task2 to use pathfinding crate

--- day10/task2/src/main.rs
+++ day10/task2/src/main.rs
@@ -1,13 +1,9 @@
 use std::{fs::File, io::Read};
 
-use pathfinding::directed::dfs::*;
+use pathfinding::directed::astar::*;
+use pathfinding::matrix::{Matrix, MatrixFormatError};
 
-type Map = Vec<Vec<usize>>;
-
-#[derive(Debug, PartialEq)]
-enum MapError {
-    InvalidMap { message: String },
-}
+type Map = Matrix<usize>;
 
 /// Reads the contents of the input file and returns a result of the file contents.
 ///
@@ -36,64 +32,69 @@ fn read_to_string(puzzle_path: &str) -> Result<String, std::io::Error> {
 ///
 /// # Returns
 ///
-/// * `Result<Vec<Vec<usize>>, MapError>` - A 2d array of positive integers or an error.
+/// * `Result<Matrix<usize>, MatrixFormatError>` - A 2d array of positive integers or an error.
 ///
-fn parse(puzzle_input: &str) -> Result<Map, MapError> {
-    let mut map = Vec::new();
-    for (line_idx, line) in puzzle_input.lines().enumerate() {
-        let mut row = Vec::new();
-        for (col_idx, c) in line.chars().enumerate() {
-            if c.is_digit(10) {
-                row.push(c.to_digit(10).unwrap() as usize);
-            } else {
-                return Err(MapError::InvalidMap {
-                    message: format!(
-                        "Invalid character '{}' at line {}, column {}",
-                        c,
-                        line_idx + 1,
-                        col_idx + 1
-                    ),
-                });
-            }
-        }
-        map.push(row);
-    }
-    Ok(map)
+fn parse(puzzle_input: &str) -> Result<Matrix<usize>, MatrixFormatError> {
+    Matrix::from_rows(
+        puzzle_input
+            .lines()
+            .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as usize)),
+    )
 }
 
-fn get_successors(map: &Map, row: usize, col: usize) -> Vec<(usize, usize)> {
-    let mut successors = Vec::new();
-    if row > 0 {
-        successors.push((row - 1, col));
-    }
-    if row < map.len() - 1 {
-        successors.push((row + 1, col));
-    }
-    if col > 0 {
-        successors.push((row, col - 1));
-    }
-    if col < map[0].len() - 1 {
-        successors.push((row, col + 1));
-    }
-    let cell = map[row][col];
-    successors
-        .into_iter()
-        .filter(|&(r, c)| map[r][c] == cell + 1)
-        .collect()
-}
-
-fn find_all_starting_points(map: &Map) -> Vec<(usize, usize)> {
-    let mut starting_points = Vec::new();
+fn find_all_points(map: &Map, point: usize) -> Vec<(usize, usize)> {
+    let mut points = Vec::new();
     for (row_idx, row) in map.iter().enumerate() {
         for (col_idx, &cell) in row.iter().enumerate() {
-            if cell == 0 {
-                starting_points.push((row_idx, col_idx));
+            if cell == point {
+                points.push((row_idx, col_idx));
             }
         }
     }
-    starting_points
+    points
 }
 
+/// Finds all paths from the start point to the end point on the given map.
+///
+/// # Arguments
+///
+/// * `start_point` - A tuple representing the starting coordinates (row, col).
+/// * `end_point` - A tuple representing the ending coordinates (row, col).
+/// * `map` - A reference to the map on which to find the paths.
+///
+/// # Returns
+///
+/// An `Option` containing a vector of vectors of tuples, where each tuple represents a coordinate (row, col) in a path.
+fn find_all_paths(
+    start_point: (usize, usize),
+    end_point: (usize, usize),
+    map: &Map,
+) -> Option<Vec<Vec<(usize, usize)>>> {
+    let successors = |&(row, col): &(usize, usize)| -> Vec<((usize, usize), usize)> {
+        let cell_value = map.get((row, col)).unwrap();
+        map.neighbours((row, col), false)
+            .filter(|&(neighbour_row, neighbour_col)| {
+                let n_cell_value = map.get((neighbour_row, neighbour_col)).unwrap();
+                (*n_cell_value == cell_value + 1) && *n_cell_value <= 9
+            })
+            .map(|n| (n, 1))
+            .collect()
+    };
+    let result = astar_bag_collect(
+        &start_point,
+        successors,
+        |_: &(usize, usize)| -> usize { 1 },
+        |&(row, col): &(usize, usize)| -> bool { (row, col) == end_point },
+    )?;
+    Some(result.0)
+}
+
+/// Prints the map with the given path highlighted.
+///
+/// # Arguments
+///
+/// * `map` - A reference to the map to be printed.
+/// * `path` - A slice of tuples representing the coordinates (row, col) of the path to be highlighted.
 fn print_map_path(map: &Map, path: &[(usize, usize)]) {
     for (row_idx, row) in map.iter().enumerate() {
         for (col_idx, &cell) in row.iter().enumerate() {
@@ -107,48 +108,23 @@ fn print_map_path(map: &Map, path: &[(usize, usize)]) {
     }
 }
 
-fn find_paths(start: (usize, usize), map: &Map) -> Vec<Vec<(usize, usize)>> {
-    let mut paths = Vec::new();
-    let successors =
-        |&(row, col): &(usize, usize)| -> Vec<(usize, usize)> { get_successors(map, row, col) };
-
-    let mut stack = vec![(vec![start], start)];
-    while let Some((path, (row, col))) = stack.pop() {
-        if map[row][col] == 9 {
-            paths.push(path.clone());
-        }
-        for succ in successors(&(row, col)) {
-            let mut new_path = path.clone();
-            new_path.push(succ);
-            stack.push((new_path, succ));
-        }
-    }
-
-    paths
-}
-
+/// The main function that reads the puzzle input, parses the map, and finds the starting points.
 fn main() {
-    let puzzle_path = "input/input.txt";
+    let puzzle_path = "input/input_example_larger.txt";
     let puzzle_input = read_to_string(puzzle_path).unwrap();
     let map = parse(&puzzle_input).unwrap();
     println!("{:?}", map);
-    let starting_points = find_all_starting_points(&map);
-    let mut all_paths = Vec::new();
-    let mut score = 0;
-    for start in starting_points {
-        let paths = find_paths(start, &map);
-        all_paths.push(paths.clone());
-        score += paths.len();
-    }
-    for (idx, paths) in all_paths.iter().enumerate() {
-        println!("Starting point {}", idx);
-        println!("{:?}", paths);
-        for path in paths {
-            print_map_path(&map, path);
-            println!();
-        }
-    }
-    println!("Score: {}", score);
+    let starting_points = find_all_points(&map, 0);
+    let ending_points = find_all_points(&map, 9);
+    let mut count = 0;
+    starting_points.iter().for_each(|&start| {
+        ending_points.iter().for_each(|&end| {
+            if let Some(paths) = find_all_paths(start, end, &map) {
+                count += paths.len();
+            }
+        });
+    });
+    println!("Count: {}", count);
 }
 
 #[cfg(test)]
@@ -165,12 +141,40 @@ mod tests {
         let map = parse(PUZZLE_INPUT).unwrap();
         assert_eq!(
             map,
-            vec![
+            Matrix::from_rows(vec![
                 vec![0, 1, 2, 3],
                 vec![1, 2, 3, 4],
                 vec![8, 7, 6, 5],
-                vec![9, 8, 7, 6]
-            ]
+                vec![9, 8, 7, 6],
+            ])
+            .unwrap()
         );
+    }
+
+    #[test]
+    fn test_find_all_points() {
+        let map = parse(PUZZLE_INPUT).unwrap();
+        let points = find_all_points(&map, 1);
+        assert_eq!(points, vec![(0, 1), (1, 0)]);
+    }
+
+    #[test]
+    fn test_find_all_paths() {
+        let map = parse(PUZZLE_INPUT).unwrap();
+        let start0 = find_all_points(&map, 0)[0];
+        let end9 = find_all_points(&map, 9)[0];
+        println!("{:?}", start0);
+        println!("{:?}", end9);
+        let paths = find_all_paths(start0, end9, &map).unwrap();
+        assert_eq!(paths.len(), 16);
+    }
+
+    #[test]
+    fn test_print_map_path() {
+        let map = parse(PUZZLE_INPUT).unwrap();
+        let start0 = find_all_points(&map, 0)[0];
+        let end9 = find_all_points(&map, 9)[0];
+        let paths = find_all_paths(start0, end9, &map).unwrap();
+        print_map_path(&map, &paths[0]);
     }
 }
