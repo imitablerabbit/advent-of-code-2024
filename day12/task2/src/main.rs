b2de7c000d6666592e24a36e8c0c5e85817729b3
Mark Hillman <mark@markhillman.info>
2024-12-12 22:44:11 +0000

    Add task2 failed implementation. Tried a permiter walk but didnt handle islands...

--- day12/task2/src/main.rs
+++ day12/task2/src/main.rs
@@ -10,8 +10,27 @@ struct Plot {
     // Coordinates of the plot. These are (row, col) coordinates.
     coords: Vec<(usize, usize)>,
 
+    // The edges in the plot. These are pairs of coordinates that make up the
+    // start and end of the edge.
+    edges: Vec<Edge>,
+
     area: usize,
     perimeter: usize,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+struct Edge {
+    start: (usize, usize),
+    end: (usize, usize),
+    direction: Direction,
 }
 
 fn read_to_string(puzzle_path: &str) -> Result<String, std::io::Error> {
@@ -55,11 +74,13 @@ fn find_plots_dimensions(map: Matrix<char>) -> Vec<Plot> {
         });
         visited.extend(reachable.iter().cloned());
         let coords: Vec<(usize, usize)> = reachable.iter().cloned().collect();
+        let edges = plot_edges(&coords);
         let area = plot_area(&coords);
         let perimeter = plot_perimeter(&coords);
         let plot = Plot {
             id: *start_char,
             coords,
+            edges,
             area,
             perimeter,
         };
@@ -73,22 +94,237 @@ fn plot_area(coords: &[(usize, usize)]) -> usize {
     coords.len()
 }
 
+/// Given a list of coordinates, returns a list of edges. An edge consists
+/// of a pair of coordinates that are the start of the edge and the end of the
+/// edge. All the edges make up the boundary of the plot. We find the edges by
+/// walking around the perimeter of the plot.
+fn plot_edges(coords: &[(usize, usize)]) -> Vec<Edge> {
+    let mut edges = vec![];
+    let coors = coords.to_vec();
+
+    // find the top left corner of the plot
+    let start_node = coors
+        .iter()
+        .fold((usize::MAX, usize::MAX), |acc, (row, col)| {
+            (acc.0.min(*row), acc.1.min(*col))
+        });
+
+    let mut move_direction = Direction::Right;
+    let mut edge_direction = Direction::Up;
+    let start_edge_direction = Direction::Up;
+
+    let mut previous = start_node;
+    let mut current = start_node;
+    let mut edge_start = start_node;
+
+    // Walk around the perimeter of the plot
+    loop {
+        let left = left_neighbour(current.0, current.1, &coors);
+        let right = right_neighbour(current.0, current.1, &coors);
+        let top = top_neighbour(current.0, current.1, &coors);
+        let bottom = bottom_neighbour(current.0, current.1, &coors);
+
+        match edge_direction {
+            Direction::Up => {
+                if top.is_some() {
+                    // We are in an inner corner. We need to change direction.
+                    let edge = Edge {
+                        start: edge_start,
+                        end: previous,
+                        direction: edge_direction,
+                    };
+                    edges.push(edge);
+                    move_direction = Direction::Up;
+                    edge_direction = Direction::Left;
+                    current = top.unwrap();
+                    edge_start = current;
+                    continue;
+                }
+            }
+
+            Direction::Right => {
+                if right.is_some() {
+                    // We are in an inner corner. We need to change direction.
+                    let edge = Edge {
+                        start: edge_start,
+                        end: previous,
+                        direction: edge_direction,
+                    };
+                    edges.push(edge);
+                    move_direction = Direction::Right;
+                    edge_direction = Direction::Up;
+                    previous = current;
+                    current = right.unwrap();
+                    edge_start = current;
+                    continue;
+                }
+            }
+            Direction::Down => {
+                if bottom.is_some() {
+                    // We are in an inner corner. We need to change direction.
+                    let edge = Edge {
+                        start: edge_start,
+                        end: previous,
+                        direction: edge_direction,
+                    };
+                    edges.push(edge);
+                    move_direction = Direction::Down;
+                    edge_direction = Direction::Right;
+                    previous = current;
+                    current = bottom.unwrap();
+                    edge_start = current;
+                    continue;
+                }
+            }
+            Direction::Left => {
+                if left.is_some() {
+                    // We are in an inner corner. We need to change direction.
+                    let edge = Edge {
+                        start: edge_start,
+                        end: previous,
+                        direction: edge_direction,
+                    };
+                    edges.push(edge);
+                    move_direction = Direction::Left;
+                    edge_direction = Direction::Down;
+                    previous = current;
+                    current = left.unwrap();
+                    edge_start = current;
+                    continue;
+                }
+            }
+        }
+
+        match move_direction {
+            Direction::Right => {
+                if right.is_some() {
+                    previous = current;
+                    current = right.unwrap();
+                } else {
+                    let edge = Edge {
+                        start: edge_start,
+                        end: current,
+                        direction: edge_direction,
+                    };
+                    edges.push(edge);
+                    move_direction = Direction::Down;
+                    edge_direction = Direction::Right;
+                    edge_start = current;
+                }
+            }
+            Direction::Down => {
+                if bottom.is_some() {
+                    previous = current;
+                    current = bottom.unwrap();
+                } else {
+                    let edge = Edge {
+                        start: edge_start,
+                        end: current,
+                        direction: edge_direction,
+                    };
+                    edges.push(edge);
+                    move_direction = Direction::Left;
+                    edge_direction = Direction::Down;
+                    edge_start = current;
+                }
+            }
+            Direction::Left => {
+                if left.is_some() {
+                    previous = current;
+                    current = left.unwrap();
+                } else {
+                    let edge = Edge {
+                        start: edge_start,
+                        end: current,
+                        direction: edge_direction,
+                    };
+                    edges.push(edge);
+                    move_direction = Direction::Up;
+                    edge_direction = Direction::Left;
+                    edge_start = current;
+                }
+            }
+            Direction::Up => {
+                if top.is_some() {
+                    previous = current;
+                    current = top.unwrap();
+                } else {
+                    let edge = Edge {
+                        start: edge_start,
+                        end: current,
+                        direction: edge_direction,
+                    };
+                    edges.push(edge);
+                    move_direction = Direction::Right;
+                    edge_direction = Direction::Up;
+                    edge_start = current;
+                }
+            }
+        }
+
+        if current == start_node && edge_direction == start_edge_direction {
+            // We have completed the loop
+            break;
+        }
+    }
+
+    edges
+}
+
+fn neighbours(row: usize, col: usize) -> Vec<(usize, usize)> {
+    let mut neighbours = vec![(row + 1, col), (row, col + 1)];
+    if col > 0 {
+        neighbours.push((row, col - 1));
+    }
+    if row > 0 {
+        neighbours.push((row - 1, col));
+    }
+    neighbours
+}
+
+fn left_neighbour(row: usize, col: usize, coords: &[(usize, usize)]) -> Option<(usize, usize)> {
+    let neighbour = (row, col.wrapping_sub(1));
+    if coords.contains(&neighbour) {
+        Some(neighbour)
+    } else {
+        None
+    }
+}
+
+fn right_neighbour(row: usize, col: usize, coords: &[(usize, usize)]) -> Option<(usize, usize)> {
+    let neighbour = (row, col + 1);
+    if coords.contains(&neighbour) {
+        Some(neighbour)
+    } else {
+        None
+    }
+}
+
+fn top_neighbour(row: usize, col: usize, coords: &[(usize, usize)]) -> Option<(usize, usize)> {
+    let neighbour = (row.wrapping_sub(1), col);
+    if coords.contains(&neighbour) {
+        Some(neighbour)
+    } else {
+        None
+    }
+}
+
+fn bottom_neighbour(row: usize, col: usize, coords: &[(usize, usize)]) -> Option<(usize, usize)> {
+    let neighbour = (row + 1, col);
+    if coords.contains(&neighbour) {
+        Some(neighbour)
+    } else {
+        None
+    }
+}
+
 /// Calculates the perimeter of a plot. This is the edges of the plot that are
 /// not shared with another plot. A 2x2 plot has a perimeter of 8.
 fn plot_perimeter(coords: &[(usize, usize)]) -> usize {
     let mut perimeter = 0;
 
     for (row, col) in coords {
-        let mut neighbours = vec![];
-        neighbours.push((row + 1, *col));
-        neighbours.push((*row, col + 1));
-        if *row > 0 {
-            neighbours.push((row - 1, *col));
-        }
-        if *col > 0 {
-            neighbours.push((*row, col - 1));
-        }
-
+        let neighbours = neighbours(*row, *col);
         let mut neighbour_count = 0;
         for (n_row, n_col) in neighbours {
             if coords.contains(&(n_row, n_col)) {
@@ -131,7 +367,7 @@ fn print_plot_shape(plot: &Plot) {
 }
 
 fn main() {
-    let puzzle_path = "input/input.txt";
+    let puzzle_path = "input/input_example2.txt";
     let puzzle_input = read_to_string(puzzle_path).unwrap();
     let map = parse(&puzzle_input).unwrap();
     let plots = find_plots_dimensions(map);
@@ -139,17 +375,18 @@ fn main() {
     let mut price = 0;
     for (i, plot) in plots.iter().enumerate() {
         println!(
-            "Plot {}: id = '{}', area = {}, perimeter = {}, coords = {:?}",
+            "Plot {}: id = '{}', area = {}, perimeter = {}, coords = {:?}, edges = {:?}",
             i + 1,
             plot.id,
             plot.area,
             plot.perimeter,
             plot.coords,
+            plot.edges
         );
         print_plot_shape(plot);
         println!();
 
-        price += plot.area * plot.perimeter;
+        price += plot.area * plot.edges.len();
     }
     println!("Total price: {}", price);
 }
